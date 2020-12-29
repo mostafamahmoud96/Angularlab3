0894ea3e0217c97a0d194d708ebb42c6c065342a
mostafamahmoud96 <moustafa.elagha96.com>
2020-12-29 23:17:23 +0200

    lab 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {BooksService} from'./services/books.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finder';
+  data: any;
+  name: any;
+  constructor(private booksService: BooksService){}
+  ngOnInit(): void {  
+  }
+  handlesearch(){
+    this.name=document.getElementById('user');
+    this.name=this.name.value;
+    this.booksService.setParram(this.name); 
+    this.booksService.getBooksList().subscribe({
+      next: (data) => {
+        data['items'].forEach(element => {
+        console.log(element.volumeInfo.title);
+        });
+        this.data=data['items']; 
+      },
+      error: (msg) => {
+        console.log('error',msg);
+      }
+    })
+  }
 }
+  
---
+++ src/app/services/books.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient}from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  title: any;
+  constructor(private http: HttpClient) { }
+
+  getBooksList() : Observable<object>{
+    return this.http.get('https://www.googleapis.com/books/v1/volumes',{
+      params:{
+        q: this.title
+      },
+    });
+  }
+  setParram(name){
+    this.title=name;
+  }
+}
